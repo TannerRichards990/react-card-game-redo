324558fb8a1a36f5c95d30fef712aac103650583
Tanner Richards <Tanner.richards990@gmail.com>
2022-10-03 11:52:13 -0700

    refactored player.js to only use gamecontext children that are needed

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import CardList from './CardList';
+import { useGameContext } from '../context/GameContext';
 
-export default function Player({
-  player,
-  setTo,
-  hand,
-  setFrom,
-  setSelectedCard,
-  selectedCard,
-  to,
-}) {
+export default function Player({ player, hand }) {
+  const { to, setTo } = useGameContext();
+  
   return (
     <div
       className={`player ${to === player ? 'selected-player' : ''}`}
@@ -19,9 +14,6 @@ export default function Player({
       <CardList
         player={player}
         cards={hand}
-        setFrom={setFrom}
-        selectedCard={selectedCard}
-        setSelectedCard={setSelectedCard}
       />
     </div>
   );
